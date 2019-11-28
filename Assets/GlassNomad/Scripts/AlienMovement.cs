39a181b54fa112c35124b6480ffe5624e47867d0
dayluke <dayluke01@gmail.com>
2019-11-28 14:24:11 +0000

    Created Alien prototype prefab (cylinder), added a basic movement script, as well as a basic health script.

---
+++ Assets/GlassNomad/Scripts/AlienMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public int speed = 10;
+    public float jumpThrust = 10;
+    private float playerHeight;
+
+    private void Start()
+    {
+
+        playerHeight = GetComponent<Collider>().bounds.extents.y;
+    }
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal") * speed;
+        float z = Input.GetAxisRaw("Vertical") * speed;
+        rb.velocity = new Vector3(x, rb.velocity.y, z);
+
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jumpThrust;
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, playerHeight + 0.1f);
+    }
+}
---
+++ Assets/GlassNomad/Scripts/PlayerHealth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth
+{
+    public int health;
+
+    public PlayerHealth(int maxHealth = 100)
+    {
+        health = maxHealth;
+    }
+
+    public void PlayerHit(int damage = 10)
+    {
+        health -= damage;
+    }
+}
