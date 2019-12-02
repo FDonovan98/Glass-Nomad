7b66183525d9891291354c7583638d188a5f7125
HDonovan96 <harry.bd11@gmail.com>
2019-12-02 08:23:50 +0000

    Imported 'PRF_LauncherUI' prefab and begun 'Launcher' script

---
+++ Assets/Scripts/PUN/Launcher.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    private byte maxPlayersPerRoom = 5;
+
+    [SerializeField]
+    private GameObject controlPanel;
+    [SerializeField]
+    private GameObject progressLabel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
