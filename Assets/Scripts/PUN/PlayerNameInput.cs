3ea24b74a830c00d72403d11d827a79d9dc9403f
HDonovan96 <harry.bd11@gmail.com>
2019-12-02 11:58:31 +0000

    Added 'PlayerNameInput' script so the player can set a nickname that is stored between sessions

---
+++ Assets/Scripts/PUN/PlayerNameInput.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using Photon.Realtime;
+using Photon.Pun;
+
+using System.Collections;
+
+[RequireComponent(typeof(InputField))]
+public class PlayerNameInput : MonoBehaviour
+{
+    private const string playerNamePrefKey = "Player Name";
+
+    void Start()
+    {
+        string defaultName = string.Empty;
+        InputField inputField = this.GetComponent<InputField>();
+
+        if (inputField != null)
+        {
+            if (PlayerPrefs.HasKey(playerNamePrefKey))
+            {
+                defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+                inputField.text = defaultName;
+            }
+        }
+
+        PhotonNetwork.NickName = defaultName;
+    }
+
+    public void SetPlayerName(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            Debug.Log("Player name is null or empty");
+            return;
+        }
+
+        PhotonNetwork.NickName = name;
+        PlayerPrefs.SetString(playerNamePrefKey, name);
+    }
+}
