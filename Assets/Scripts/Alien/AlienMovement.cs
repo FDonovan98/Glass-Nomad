51222d63e78c783dbb22b05f9dea725a6b19a036
HDonovan96 <harry.bd11@gmail.com>
2019-12-03 10:59:12 +0000

    In 'AlienMovement' script added public variables

--- Assets/Scripts/Alien/AlienMovement.cs
+++ Assets/Scripts/Alien/AlienMovement.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Code initially based on code from here:
+// https://answers.unity.com/questions/155907/basic-movement-walking-on-walls.html
 public class AlienMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float movementSpeed = 6;
+    // Turn speed is in degrees per second.
+    public float turnSpeed = 90; 
+    // Smoothing speed.
+    public float lerpSpeed = 10;
+    public float gravity = 10;
+    // Is the alien in contact with the ground.
+    public bool isGrounded;
+    // The initial speed of a jump.
+    public float jumpSpeed = 10;
+    // The range at which to detect a wall to stick to.
+    public float jumpRange = 10;
 }
