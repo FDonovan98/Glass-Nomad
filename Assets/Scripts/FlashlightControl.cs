d88a75f9681bfc8e13521b4ee40c89cef4d4f8a9
Eugene Syrickis <js231622@falmouth.ac.uk>
2019-12-03 15:32:18 +0000

    Made a basic flashlight prefab and a script to turn it on and off by pressing F for now.

---
+++ Assets/Scripts/FlashlightControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightControl : MonoBehaviour
+{
+    private Light flashlight;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        flashlight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            flashlight.enabled = !flashlight.enabled;
+        }
+    }
+}
