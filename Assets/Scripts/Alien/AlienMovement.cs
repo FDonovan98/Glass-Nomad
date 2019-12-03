0d5da07ea9fb6d500a417c595251e3480bcc3aaa
HDonovan96 <harry.bd11@gmail.com>
2019-12-03 12:06:48 +0000

    In 'AlienMovement' script added 'Start' function to initialise

--- Assets/Scripts/Alien/AlienMovement.cs
+++ Assets/Scripts/Alien/AlienMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 // Code initially based on code from here:
 // https://answers.unity.com/questions/155907/basic-movement-walking-on-walls.html
+
+[RequireComponent(typeof(Collider))]
 public class AlienMovement : MonoBehaviour
 {
     public float movementSpeed = 6;
@@ -21,10 +23,23 @@ public class AlienMovement : MonoBehaviour
 
     // The normal of the current surface.
     private Vector3 surfaceNormal;
+    // The characters normal.
+    private Vector3 charNormal;
     // The distance of the Alien from the ground.
     private float distGround;
     // Flag for if the alien is currently jumping.
     private bool jumping;
     // Current vertical speed.
     private float verticalSpeed;
+    private Collider charCollider;
+
+    void Start()
+    {
+        // Gets the character collider.
+        charCollider = this.GetComponent<Collider>();
+        // Initialises the charNormal to the world normal.
+        charNormal = transform.up;
+        // Gets the height from the centre of the collider to the ground.
+        distGround = charCollider.bounds.extents.y - charCollider.bounds.center.y;
+    }
 }
