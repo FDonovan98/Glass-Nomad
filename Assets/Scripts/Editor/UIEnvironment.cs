5759b31915f28eeeb7d7cab9cd82d6c898802762
HDonovan96 <harry.bd11@gmail.com>
2019-12-12 14:30:45 +0000

    Begun 'UIEnvironment' script to enable an editor window to control the ui and environment

---
+++ Assets/Scripts/Editor/UIEnvironment.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+
+using UnityEngine;
+
+public class UIEnvironment : EditorWindow
+{
+    bool showHealthBars = false;
+    [MenuItem("Window/UI and Environment")]
+    public static void ShowWindow()
+    {
+        GetWindow<UIEnvironment>("UI and Environment");
+    }
+
+    void OnGUI()
+    {
+        showHealthBars = EditorGUILayout.Toggle("Enable Health Bars", showHealthBars);
+    }
+
+    void DisableHealthBars()
+    {
+        GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
+    }
+}
