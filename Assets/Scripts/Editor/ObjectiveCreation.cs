ea8d3bf82d4d37e262d7845dca15d4826fc00a08
HDonovan96 <harry.bd11@gmail.com>
2019-12-17 16:33:42 +0000

    In 'Objective Creation' window added trigger selection

--- Assets/Scripts/Editor/ObjectiveCreation.cs
+++ Assets/Scripts/Editor/ObjectiveCreation.cs
@@ -1,10 +1,57 @@
 ﻿using UnityEditor;
 
+using UnityEngine;
+using UnityEngine.UI;
+
 public class ObjectiveCreation : EditorWindow
-{
+{    
+    int chosenTrigger = 0;
+    Button button;
+
     [MenuItem("Window/Dev Tools/Objective Creation")]
     public static void ShowWindow()
     {
         GetWindow<ObjectiveCreation>("Objective Creation");
+    }
+
+    void OnGUI()
+    {
+        Triggers();
+        Effects();
+    }
+
+    // Renders menu items handling the selection of triggers.
+    private void Triggers()
+    {
+        string[] triggerOptions = 
+        {
+            "On Button Press",
+            "On Bool is True",
+            "On Area Entered"
+        };
+
+        chosenTrigger = EditorGUI.Popup
+        (
+            new Rect(0, 0, 270, 20), 
+            "Trigger:",
+            chosenTrigger,
+            triggerOptions
+        );
+
+        switch (chosenTrigger)
+        {
+            case 0:
+                EditorGUILayout.ObjectField("Trigger Button", button, typeof(Button), true);
+                return;
+
+            default:
+                return;
+        }
+    }
+
+    // Renders menu items handling the effect of the trigger.
+    private void Effects()
+    {
+
     }
 }
