93105f8a8dc1e730030401a2091fb78990dd11a0
HDonovan96 <harry.bd11@gmail.com>
2020-01-07 14:03:19 +0000

    In 'WeaponClass' script added constructor

--- Assets/Scripts/General Scripts/WeaponClass.cs
+++ Assets/Scripts/General Scripts/WeaponClass.cs
@@ -1,4 +1,26 @@
 ﻿public class WeaponClass
 {
-    
+    // Number of magazines for the weapon.
+    public int magCount;
+
+    protected int bulletsInCurrentMag;
+
+    // Rate of fire in rounds per second.
+    private float fireRate;
+    // Max number of bullets stored in a magazine.
+    private int magSize;
+    private float range;
+    private float damage;
+
+    protected WeaponClass(int magazineCount, float rateOfFire, int magazingSize, float weaponRange, float weaponDamage)
+    {
+        magCount = magazineCount;
+        fireRate = rateOfFire;
+        magSize = magazingSize;
+        range = weaponRange;
+        damage = weaponDamage;
+
+        // Starts the weapon with a full magazine.
+        bulletsInCurrentMag = magSize;
+    }
 }
