a3d0c4fe02900b4875db678c1756f12890ec76c6
HDonovan96 <harry.bd11@gmail.com>
2020-01-07 15:12:17 +0000

    'WeaponClass' now checks if weapon can fire and casts a ray if it can

--- Assets/Scripts/General Scripts/WeaponClass.cs
+++ Assets/Scripts/General Scripts/WeaponClass.cs
@@ -1,26 +1,59 @@
-﻿public class WeaponClass
+﻿using UnityEngine;
+
+public class WeaponClass
 {
+    public enum FireType
+    {
+        Single,
+        Burst,
+        FullAuto
+    }
     // Number of magazines for the weapon.
     public int magCount;
+    public FireType fireMode;
 
     protected int bulletsInCurrentMag;
 
-    // Rate of fire in rounds per second.
+    // inimum time delay between each shot.
     private float fireRate;
     // Max number of bullets stored in a magazine.
     private int magSize;
     private float range;
     private float damage;
 
-    protected WeaponClass(int magazineCount, float rateOfFire, int magazingSize, float weaponRange, float weaponDamage)
+    protected WeaponClass(int magazineCount, float shotsPerSecond, int magazingSize, float weaponRange, float weaponDamage)
     {
         magCount = magazineCount;
-        fireRate = rateOfFire;
+        fireRate = 1 / shotsPerSecond;
         magSize = magazingSize;
         range = weaponRange;
         damage = weaponDamage;
 
         // Starts the weapon with a full magazine.
         bulletsInCurrentMag = magSize;
+    }
+
+    protected void FireWeapon(ref float deltaTime, GameObject camera)
+    {
+        if (canFire(deltaTime))
+        {
+            deltaTime -= fireRate;
+
+            RaycastHit hit;
+            if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, range))
+            {
+
+            }
+        }
+    }
+
+    private bool canFire(float deltaTime)
+    {
+        if (deltaTime > fireRate)
+        {
+            return true;
+        }
+
+        return false;
     }
 }
