c7ff1b14680fe48643a60d054674b9a7b1ced908
HDonovan96 <harry.bd11@gmail.com>
2020-01-15 10:46:00 +0000

    Begun 'ObjectInteraction' script

---
+++ Assets/Scripts/General Scripts/ObjectInteraction.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ObjectInteraction : MonoBehaviour
+{
+    // Time taken for the action to complete.
+    public float actionDuration;
+    public GameObject actionArea;
+
+    // Time the action has been going on for.
+    private float interactionDuration;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            MarineMovement marineMovement = other.gameObject.GetComponent<MarineMovement>();
+        }
+    }
+}
