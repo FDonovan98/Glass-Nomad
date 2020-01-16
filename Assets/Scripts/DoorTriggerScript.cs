eaf4782c5c3bb3d69769939e781ecd61e355ad18
Luke Day <ld227707@falmouth.ac.uk>
2020-01-16 19:27:16 +0000

    Added trigger animations to the doors. This also syncs across network.

---
+++ Assets/Scripts/DoorTriggerScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+public class DoorTriggerScript : MonoBehaviourPunCallbacks
+{
+    private Animator anim;
+    private bool isDoorOpen = false;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            // ---- might need to check that this is a local player ----
+            // ---- might want a button press to trigger the below? ----
+            ChangeDoorState();
+        }
+    }
+
+    private void OnTriggerExit(Collider coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            // ---- might need to check that this is a local player ----
+            ChangeDoorState();
+        }
+    }
+
+    private void ChangeDoorState()
+    {
+        isDoorOpen = !isDoorOpen;
+        Debug.Log(isDoorOpen ? "Door opening" : "Door closing");
+        anim.SetBool("doorOpen", isDoorOpen);
+    }
+}
