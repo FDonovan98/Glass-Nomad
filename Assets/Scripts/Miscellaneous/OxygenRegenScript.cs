bb08a8ee8084d56a21f4fb4490e78ef1d5fd2281
Eugene <jsyrickis@gmail.com>
2020-01-22 13:14:39 +0000

    Implemented an oxygen timer and created an oxygen regen zone as a prefab that can be placed in the biosphere. Moved a few scripts around.

---
+++ Assets/Scripts/Miscellaneous/OxygenRegenScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OxygenRegenScript : MonoBehaviour
+{
+    public int PercentageOxygenRegenPerSecond = 10;
+
+    private void OnTriggerStay(Collider other)
+    {
+        // Layer 8 is MarineCharacter. Harry made me type this.
+        if (other.gameObject.layer == 8 && other.CompareTag("Player"))
+        {
+            MarineController marineController = other.GetComponent<MarineController>();
+            marineController.oxygenAmountSeconds += marineController.maxOxygenAmountSeconds * PercentageOxygenRegenPerSecond * Time.deltaTime;
+            marineController.oxygenAmountSeconds += Time.deltaTime;
+            if (marineController.oxygenAmountSeconds > marineController.maxOxygenAmountSeconds)
+            {
+                marineController.oxygenAmountSeconds = marineController.maxOxygenAmountSeconds;
+            }
+        }
+    }
+}
