e1b8ce683ca4a63e266fc9af72246705cfce7c42
Luke Day <ld227707@falmouth.ac.uk>
2020-01-31 00:10:44 +0000

    Commented the red switch trigger script.

--- Assets/Scripts/Miscellaneous/RedSwitchTriggerScript.cs
+++ Assets/Scripts/Miscellaneous/RedSwitchTriggerScript.cs
@@ -1,19 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class RedSwitchTriggerScript : MonoBehaviour
 {
-    public RedSwitchManager switchManager = null;
+    // Tells the red switch manager when this switch has been (de)activated.
+    [SerializeField] private RedSwitchManager switchManager = null;
+
+    // The current time that the switch has been held for.
     private float currentTime = 0f;
+
+    // The amount of time it takes for the switch to activate.
     private float timeToOpen = 5f;
+
+    // Whether the switch is currently activated or not.
     private bool switchActivated = false;
 
     private void OnTriggerEnter()
     {
+        // Resets the timer to activate the switch.
         currentTime = 0f;
     }
 
+    /// <summary>
+    /// Once the player enters the switch's collider and their holding 'E',
+    /// the timer is started. If the player successfully holds the switch for
+    /// the duration of the timer, then the switch is activated. If the player
+    /// releases the switch, then it is deactivated and will need to be activated
+    /// again.
+    /// </summary>
+    /// <param name="coll"></param>
     private void OnTriggerStay(Collider coll)
     {
         if (coll.gameObject.tag == "Player")
@@ -32,8 +46,10 @@ public class RedSwitchTriggerScript : MonoBehaviour
                         switchManager.SwitchActivated();
                     }
                 }
-            } else // if the player is not pressing then reset the switch's state.
+            }
+            else
             {
+                // If the player is not pressing then reset the switch's state.
                 currentTime = 0f;
                 if (switchActivated)
                 {
@@ -45,6 +61,10 @@ public class RedSwitchTriggerScript : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// If the player exits the switch's collider, then reset the 
+    /// switch's state and timer.
+    /// </summary>
     private void OnTriggerExit()
     {
         currentTime = 0f;
