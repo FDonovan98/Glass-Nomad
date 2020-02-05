982617787e48483b3f9ab7a75462083a0173ba91
dayluke <dayluke01@gmail.com>
2020-02-05 13:10:45 +0000

    Commented the objective script.

--- Assets/Scripts/Miscellaneous/Objectives.cs
+++ Assets/Scripts/Miscellaneous/Objectives.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Objectives : MonoBehaviour
 {
+    // The CSV file to read the objectives from.
     private string csvPath = "Assets/Scripts/Miscellaneous/objectivesTextFile.csv";
+
+    // A list of all of the objectives.
     private static List<Objective> objectives = new List<Objective>();
 
+    /// <summary>
+    /// Reads all the data from the CSV file, and calls the 'START' objective.
+    /// </summary>
     private void Start()
     {
         ReadData();
-        ObjectiveComplete("START");
+        ObjectiveComplete("START"); // Displays the starting text.
     }
 
+    /// <summary>
+    /// Loops through every line in the CSV file, finds the first comma, and splits it into 
+    /// a string array. The first item in the array is the objective's name/title, and the 
+    /// second item is the objective's dialogue/description. These objectives are then added
+    /// to the 'objectives' list.
+    /// </summary>
     private void ReadData()
     {
         using (StreamReader reader = new StreamReader(csvPath))
@@ -20,12 +32,18 @@ public class Objectives : MonoBehaviour
             while (!reader.EndOfStream)
             {
                 string line = reader.ReadLine();
-                string[] data = line.Split(',');
+                string[] data = line.Split(new[] { ',' }, 2);
                 objectives.Add(new Objective(data[0], data[1]));
             }
         }
     }
 
+    /// <summary>
+    /// Static method, so it can be called by other scripts, without needing to hold a reference
+    /// to it. Attempts to find the objective name passed through the parameters. If an objective
+    /// with the same title is found, then the 'Say' method is called.
+    /// </summary>
+    /// <param name="objName">The objective's title that has been completed.</param>
     public static void ObjectiveComplete(string objName)
     {
         Objective objectiveCompleted = new Objective("", "");
@@ -37,12 +55,22 @@ public class Objectives : MonoBehaviour
     }
 }
 
+/// <summary>
+/// A small class that holds the title, dialogue and boolean of whether the objective has been
+/// completed or not.
+/// </summary>
 public class Objective
 {
     public string title;
     public string dialogue;
     public bool completed;
 
+    /// <summary>
+    /// Constructor method of this class. Assigns the title and dialogue strings, and the completed
+    /// boolean.
+    /// </summary>
+    /// <param name="ttl"></param>
+    /// <param name="diag"></param>
     public Objective(string ttl, string diag)
     {
         title = ttl;
@@ -50,6 +78,10 @@ public class Objective
         completed = false;
     }
 
+    /// <summary>
+    /// Called when an objective is completed.
+    /// Displays the dialogue of the objective and marks it as completed.
+    /// </summary>
     public void Say()
     {
         Debug.Log(dialogue);
