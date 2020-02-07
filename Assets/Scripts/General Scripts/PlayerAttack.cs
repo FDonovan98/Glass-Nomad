ae1f5a4f0221e51a01f001132dd9ab4da12f850c
dayluke <dayluke01@gmail.com>
2020-02-07 15:02:53 +0000

    Re-commented and refactored the player attack script.

--- Assets/Scripts/General Scripts/PlayerAttack.cs
+++ Assets/Scripts/General Scripts/PlayerAttack.cs
@@ -1,40 +1,62 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
 using System.Collections;
-using System;
 
 public class PlayerAttack : MonoBehaviourPunCallbacks
 {
-    // Used to change the health bar slider above the player.
-    [SerializeField]
-    public Image healthSlider = null;
+    #region variable-declaration
 
     // Used to set the player's health the max, on initialisation.
-    [SerializeField]
-    private int maxHealth = 100;
+    [SerializeField] private int maxHealth = 100;
+
+    // Used to change the health bar slider above the player.
+    public Image healthSlider = null;
 
     // Used to control the health of this player.
     public PlayerResources resourcesScript;
+
     // Used to disable/enable the camera so that we only control our local player's camera.
     private GameObject cameraGO;
 
-    private float deltaTime = 0.0f;
+    // Used to control the rate of fire, reload time, audio clip, etc.
     public Weapon currentWeapon;
-    private float recoil = 0f;
-    private float recoil_rotation = 0f;
 
+    // Used to play the current weapons audio clip.
+    private AudioSource weaponAudio;
+
+    // Both used to recoil the players weapon.
+    private float recoil = 0f;
+    private float recoilRotation = 0f;
+
+    // Used to keep track of how long it has been since the weapon was last fired.
+    private float deltaTime = 0.0f;
+
+    // Used to spawn a muzzle flash when a player shoots.
     private MuzzleFlashScript muzzleFlash;
-    private Vector3 muzzleFlashPosition;
+
+    // Used to position the muzzle flash.
     private Light flashlight;
 
-    public GameObject bulletHolePrefab; // Spawned when a bullet hits a wall.
+    // Used to display the players current ammo, mag count, and oxygen.
+    private UIBehaviour hudCanvas;
 
+    // Spawned when a bullet hits a wall.
+    public GameObject bulletHolePrefab;
+
+    #endregion
+
+    /// <summary>
+    /// Initialises the players resources. 
+    /// </summary>
     private new void OnEnable()
     {
         resourcesScript = new PlayerResources(this.gameObject, this, maxHealth);
     }
+
+    /// <summary>
+    /// Assigns the flashlight, camera, weapon audio and HUD canvas.
+    /// </summary>
     private void Start()
     {
         resourcesScript.magsLeft = currentWeapon.magCount;
@@ -49,6 +71,10 @@ public class PlayerAttack : MonoBehaviourPunCallbacks
 
         // Gets the camera child on the player.
         cameraGO = this.GetComponentInChildren<Camera>().gameObject;
+
+        weaponAudio = cameraGO.GetComponentInChildren<AudioSource>();
+        weaponAudio.clip = currentWeapon.weaponSound;
+        currentWeapon.magsLeft = currentWeapon.magCount;
         deltaTime = currentWeapon.fireRate;
 
         resourcesScript.hudCanvas.UpdateUI(GetComponent<PlayerAttack>());
@@ -56,103 +82,85 @@ public class PlayerAttack : MonoBehaviourPunCallbacks
 
     private void Update()
     {
-        if (!photonView.IsMine)
-        {
-            return;
-        }
+        if (!photonView.IsMine) return;
 
-        deltaTime += Time.deltaTime;
+        if (deltaTime <= currentWeapon.fireRate) deltaTime += Time.deltaTime;
 
-        if (Input.GetButton("Fire1"))
+        if (currentWeapon.CanFire(deltaTime))
         {
-            if (canFire(deltaTime, currentWeapon))
+            if (currentWeapon.fireMode == Weapon.FireType.Single)
             {
-                // Calls the 'Attack' method on all clients, meaning that the health will be synced across all clients.
-                photonView.RPC("FireWeapon", RpcTarget.All, cameraGO.transform.position, cameraGO.transform.forward, currentWeapon.range, currentWeapon.damage);
-
-                // If magSize is zero then it is a melee attack.
-                if (currentWeapon.magSize > 0)
+                if (Input.GetButtonDown("Fire1"))
                 {
-                    resourcesScript.UpdatePlayerResource(PlayerResources.PlayerResource.Ammo, -1);
-                    recoil += currentWeapon.recoilForce;
-
-                    if (flashlight != null)
-                    {
-                        muzzleFlashPosition = flashlight.gameObject.transform.position;
-                    }
+                    Shoot();
                 }
-
-                if (muzzleFlash != null)
-                {
-                    StartCoroutine(muzzleFlash.Flash(muzzleFlashPosition, flashlight.gameObject.transform.rotation));
-                }
-
-                deltaTime = 0;
             }
+            else if (currentWeapon.fireMode == Weapon.FireType.FullAuto)
+            {
+                if (Input.GetButton("Fire1"))
+                {
+                    Shoot();
+                }
+            }
+
+            deltaTime = 0;
         }
 
-        if (recoil > 0)
-        {
-            RecoilWeapon();
-        }
-        
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            ReloadWeapon();
-        }
+        if (recoil > 0) { RecoilWeapon(); }
 
-        // Reduce oxygen
+        if (Input.GetKeyDown(KeyCode.R)) currentWeapon.Reload();
+
+        ReduceOxygen();
+    }
+
+    /// <summary>
+    /// Used to call the PunRPC 'FireWeapon' so that every clients registers the fire, and takes damage accordingly.
+    /// This also reduces the players ammo count and adds recoil, as well as spawning the muzzle flash.
+    /// </summary>
+    private void Shoot()
+    {
+        // Calls the 'FireWeapon' method on all clients, meaning that the health and gun shot will be synced across all clients.
+        photonView.RPC("FireWeapon", RpcTarget.All, cameraGO.transform.position, cameraGO.transform.forward,
+                    currentWeapon.range, currentWeapon.damage);
+
+        currentWeapon.bulletsInCurrentMag--;
+        recoil += currentWeapon.recoilForce;
+
+        if (muzzleFlash != null)
+        {
+            StartCoroutine(muzzleFlash.Flash(flashlight.gameObject.transform.position, flashlight.gameObject.transform.rotation));
+        }
+    }
+
+    /// <summary>
+    /// Used to recoil the players weapon when they shoot.
+    /// </summary>
+    private void RecoilWeapon()
+    {
+        float xRotation = cameraGO.transform.localEulerAngles.x;
+        recoil *= 10 * Time.deltaTime; // This dampens the recoil until it is (almost) zero.
+        recoilRotation += recoil;
+        recoilRotation *= 0.95f; // Gets smaller every frame.
+        cameraGO.transform.localEulerAngles = new Vector3(xRotation - recoilRotation, cameraGO.transform.localEulerAngles.y, cameraGO.transform.localEulerAngles.z);
+    }
+
+    private void ReduceOxygen()
+    {
         if (resourcesScript.oxygenAmountSeconds > 0)
         {
             resourcesScript.UpdatePlayerResource(PlayerResources.PlayerResource.OxygenLevel, -Time.fixedDeltaTime);
         }
     }
 
-    private void ReloadWeapon()
-    {
-        if (resourcesScript.magsLeft > 0)
-        {
-            resourcesScript.UpdatePlayerResource(PlayerResources.PlayerResource.Ammo, currentWeapon.magSize);
-            resourcesScript.UpdatePlayerResource(PlayerResources.PlayerResource.Magazines, -1);
-        }
-        else
-        {
-            Debug.Log("You are out of magazines for this weapon. Find more ammo.");
-        }
-    }
-
-    private void RecoilWeapon()
-    {
-        float xRotation = cameraGO.transform.localEulerAngles.x;
-        recoil *= 10 * Time.deltaTime; // this dampens the recoil until it is (almost) zero.
-        recoil_rotation += recoil;
-        recoil_rotation *= 0.95f; // get smaller every frame.
-        cameraGO.transform.localEulerAngles = new Vector3(xRotation - recoil_rotation, cameraGO.transform.localEulerAngles.y, cameraGO.transform.localEulerAngles.z);
-    }
-
-    private bool canFire(float deltaTime, Weapon weapon)
-    {
-        if (deltaTime > weapon.fireRate)
-        {
-            if (weapon.magSize > 0)
-            {
-                if (resourcesScript.bulletsInCurrentMag > 0)
-                {
-                    return true;
-                }
-                else
-                {
-                    Debug.Log("You are out of bullets in your magazine.");
-                }
-                return false;
-            }  
-            return true; 
-        }
-        
-        return false;
-        
-    }
-
+    /// <summary>
+    /// Plays a gun shot sound, so that all clients hear it. If the bullet hits a player,
+    /// then the hit player's health is reduced, otherwise, if the bullet hit a wall, then
+    /// a bullet hole is instantiated and faded out.
+    /// </summary>
+    /// <param name="cameraPos"></param>
+    /// <param name="cameraForward"></param>
+    /// <param name="range"></param>
+    /// <param name="damage"></param>
     [PunRPC]
     protected void FireWeapon(Vector3 cameraPos, Vector3 cameraForward, float range, int damage)
     {
@@ -180,6 +188,12 @@ public class PlayerAttack : MonoBehaviourPunCallbacks
         }
     }
 
+    /// <summary>
+    /// Used to fade out the bullet if it hits a wall.
+    /// </summary>
+    /// <param name="bullet"></param>
+    /// <param name="fadeDuration"></param>
+    /// <returns></returns>
     IEnumerator FadeBulletOut(GameObject bullet, float fadeDuration)
     {
         Color col = bullet.GetComponent<MeshRenderer>().material.color;
