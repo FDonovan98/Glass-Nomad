b379e34523a83d34e80827b0c4fd77d75a4cde4a
HDonovan96 <harry.bd11@gmail.com>
2020-02-12 20:14:38 +0000

    'LightSequencing' now flashes lights

--- Assets/Scripts/Miscellaneous/LightSequencing.cs
+++ Assets/Scripts/Miscellaneous/LightSequencing.cs
@@ -7,26 +7,34 @@ public class LightSequencing : MonoBehaviour
     public List<Light> lights = new List<Light>();
     public Color startingColour = Color.red;
     public Color endColour = Color.green;
-    public float timeOnStartColour = 10.0f;
-
-    private WaitForSeconds timeDelay;
-    private IEnumerator coroutine;
+    public float timeBetweenFlashes = 1.0f;
+    public float timeUntilFlashingEnds = 10.0f;
+    private Color targetColor;
 
     // Start is called before the first frame update
     void Start()
     {
-        timeDelay = new WaitForSeconds(0.0f);
-        ChangeLightColour(startingColour, timeDelay);
+        targetColor = endColour;
+        ChangeLightColour();
 
-        timeDelay = new WaitForSeconds(timeOnStartColour);
-        coroutine = ChangeLightColour(endColour, timeDelay);
-        
-        StartCoroutine(coroutine);
+        Invoke("EndRepeating", timeUntilFlashingEnds);
+        InvokeRepeating("ChangeLightColour", timeBetweenFlashes, timeBetweenFlashes);
     }
 
-    private IEnumerator ChangeLightColour(Color targetColor, WaitForSeconds delay)
+    private void EndRepeating()
     {
-        yield return delay;
+        targetColor = endColour;
+        foreach (Light element in lights)
+        {
+            element.color = targetColor;
+        }
+
+        CancelInvoke("ChangeLightColour");
+    }
+
+    private void ChangeLightColour()
+    {
+        targetColor = (targetColor == startingColour) ? endColour : startingColour;
 
         foreach (Light element in lights)
         {
