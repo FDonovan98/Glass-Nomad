9ecf3cd14cf790c7763a840a6f63a1d455b3ff6b
dayluke <dayluke01@gmail.com>
2020-02-13 14:08:39 +0000

    Re-added the sliders and toggle menu options to the game manager in the blockout scene.

--- Assets/Scripts/General Scripts/ReticleProgress.cs
+++ Assets/Scripts/General Scripts/ReticleProgress.cs
@@ -6,7 +6,7 @@ public static class ReticleProgress
     public static void UpdateReticleProgress(float currProgress, Image outerReticle)
     {
         outerReticle.fillAmount = (currProgress / 100);
-        Debug.Log("CURR PROGRESS: " + outerReticle.fillAmount);
+        // Debug.Log("CURR PROGRESS: " + outerReticle.fillAmount);
         if (currProgress >= 100)
         {
             outerReticle.fillAmount = 0;
