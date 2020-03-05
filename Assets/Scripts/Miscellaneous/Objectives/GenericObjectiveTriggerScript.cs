ea119841e4c060f39ae40cf58eb6f07e90cf54a3
dayluke <dayluke01@gmail.com>
2020-03-05 12:19:30 +0000

    Added an objective trigger prefab and altered the generic objective trigger script slightly.

--- Assets/Scripts/Miscellaneous/Objectives/GenericObjectiveTriggerScript.cs
+++ Assets/Scripts/Miscellaneous/Objectives/GenericObjectiveTriggerScript.cs
@@ -6,13 +6,17 @@ public class GenericObjectiveTriggerScript : TriggerInteractionScript
     [SerializeField] private string objectiveName = "";
     [SerializeField] private string objectiveRequired = "";
     [SerializeField] private bool destroyObjectAfter = true;
+    [SerializeField] private GameObject objectToDestroy = null;
 
     protected override void InteractionComplete(GameObject player)
     {
         Objectives.ObjectiveComplete(objectiveName, objectiveRequired);
+        
+        GetComponent<Collider>().enabled = false;
         if (destroyObjectAfter)
         {
-            PhotonNetwork.Destroy(gameObject);
+            if (objectToDestroy == null) PhotonNetwork.Destroy(gameObject);
+            else PhotonNetwork.Destroy(objectToDestroy);
         }
     }
 }
