2d777c0e489cd909659daeb9178e74704dc6be7e
dayluke <Appl5467>
2020-03-05 11:53:22 +0000

    Moved some of the trigger interaction scripts into a separate folder and created a new 'GenericObjectiveTriggerScript'.

---
+++ Assets/Scripts/Miscellaneous/Objectives/GenericObjectiveTriggerScript.cs
@@ -0,0 +1,18 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class GenericObjectiveTriggerScript : TriggerInteractionScript
+{
+    [SerializeField] private string objectiveName = "";
+    [SerializeField] private string objectiveRequired = "";
+    [SerializeField] private bool destroyObjectAfter = true;
+
+    protected override void InteractionComplete(GameObject player)
+    {
+        Objectives.ObjectiveComplete(objectiveName, objectiveRequired);
+        if (destroyObjectAfter)
+        {
+            PhotonNetwork.Destroy(gameObject);
+        }
+    }
+}
