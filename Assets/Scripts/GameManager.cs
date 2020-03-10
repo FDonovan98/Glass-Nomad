a98330df3d02576944ecdb8622a66f6a88d7c5b9
dayluke <dayluke01@gmail.com>
2020-03-10 17:51:38 +0000

    Fixed marine spawning

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -75,11 +75,11 @@ public class GameManager : MonoBehaviourPunCallbacks, IInRoomCallbacks
 
     private Vector3 GetRandomSpawnPoint()
     {
-        UnityEngine.Random.InitState((int)Time.time);
         Vector3 pos = marineSpawnPoint.transform.position;
-        Vector2 circle = UnityEngine.Random.insideUnitCircle * 3;
-        pos.x += circle.x;
-        pos.z += circle.y;
+        // Vector2 circle = UnityEngine.Random.insideUnitCircle * 3;
+        // pos.x += circle.x;
+        // pos.z += circle.y;
+        Debug.Log(pos);
         return pos;
     }
 
