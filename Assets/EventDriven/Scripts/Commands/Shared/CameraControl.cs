44f36bd388a9d31935ea9679502c30be9158326c
HDonovan96 <harry.bd11@gmail.com>
2020-03-20 13:45:31 +0000

    Added camera control command

---
+++ Assets/EventDriven/Scripts/Commands/Shared/CameraControl.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "DefaultCameraMovement", menuName = "Commands/Camera Controls")]
+public class CameraControl : CommandObject
+{
+    protected override void OnEnable()
+    {
+
+    }
+
+    public override void Execute(GameObject agent, MovementValues movementValues)
+    {
+        Vector3 mouseRotationInput = GetMouseInput(agent);
+
+        // Agent Rotation.
+        Vector3 agentRotation = new Vector3(0.0f, mouseRotationInput.x, 0.0f);
+        agentRotation *= movementValues.mouseSensitivity;
+        agent.transform.Rotate(agentRotation);
+
+        // Camera Rotation.
+        Camera agentCamera = agent.GetComponentInChildren<Camera>();
+        Quaternion cameraTargetRotation = agentCamera.transform.localRotation;
+
+        float cameraRotation = -mouseRotationInput.y * movementValues.mouseSensitivity;
+        cameraTargetRotation *= Quaternion.Euler(cameraRotation, 0.0f, 0.0f);
+        cameraTargetRotation = ClampRotationAroundXAxis(cameraTargetRotation, movementValues);
+
+        agentCamera.transform.localRotation = cameraTargetRotation;
+    }
+
+    private Vector3 GetMouseInput(GameObject agent)
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        return new Vector3(mouseX, mouseY, 0);
+    }
+
+    private Quaternion ClampRotationAroundXAxis(Quaternion q, MovementValues movementValues)
+    {
+        // Quaternion is 4x4 matrix.
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1.0f;
+
+        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
+
+        angleX = Mathf.Clamp(angleX, -movementValues.yRotationClamp, movementValues.yRotationClamp);
+
+        // Updates x.
+        q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+
+        return q;
+    }
+}
--- Assets/EventDriven/Scripts/Commands/Shared/MovementValues.cs
+++ Assets/EventDriven/Scripts/Commands/Shared/MovementValues.cs
@@ -3,10 +3,15 @@
 [CreateAssetMenu(fileName = "DefaultMovementValues", menuName = "Commands/Movement Values")]
 public class MovementValues : ScriptableObject
 {
+    [Header("XZ Movement")]
     public float moveSpeed = 1.0f;
     public bool sprintingIsAToggle;
     public bool isSprinting = false;
     public float sprintMultiplier = 2.0f;
+
+    [Header("Camera")]
+    public float mouseSensitivity = 1.0f;
+    public float yRotationClamp = 80.0f;
 
     public void Initialise()
     {
