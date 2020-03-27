5505ae43ba96def4775c3ce02144a87e106d0e8a
HDonovan96 <harry.bd11@gmail.com>
2020-03-27 14:34:59 +0000

    Started working on a spider climb ability module

---
+++ Assets/EventDriven/Scripts/Commands/Passive/SpiderClimb.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DefaultSpiderClimb", menuName = "Commands/Order/SpiderClimb")]
+public class SpiderClimb : PassiveCommandObject
+{
+    public override void RunCommandOnStart(AgentInputHandler agentInputHandler)
+    {
+        
+    }
+}
