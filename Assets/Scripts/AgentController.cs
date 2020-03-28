bece1592a6b2a29080447e3e68f1a008e33e0c44
HDonovan96 <harry.bd11@gmail.com>
2020-03-28 08:23:05 +0000

    Begun a command to disable objects for photon

---
+++ Assets/Scripts/AgentController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentController : MonoBehaviourPunCallbacks
+{
+    public delegate void RunCommandOnAwake();
+    public RunCommandOnAwake runCommandOnAwake;
+
+    void Awake()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Commands/Controller/DisableObjectsForPhoton.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DefaultDisableObjectsForPhoton", menuName = "Commands/Controller/DisableObjectsForPhoton", order = 0)]
+public class DisableObjectsForPhoton : ScriptableObject
+{
+
+}
