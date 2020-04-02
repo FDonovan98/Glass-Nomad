0d39a14968c7ad2e83bc5489a720d9afbe1b23dd
Luke Day <ld227707@falmouth.ac.uk>
2020-04-02 15:36:28 +0100

    Fixed the agent hit effects

--- Assets/Scripts/Commands/Agents/Passive/AgentGivesHitFeedback.cs
+++ Assets/Scripts/Commands/Agents/Passive/AgentGivesHitFeedback.cs
@@ -9,8 +9,17 @@ public class AgentGivesHitFeedback : PassiveCommandObject
         agentInputHandler.runCommandOnAgentHasBeenHit += RunCommandOnAgentHasBeenHit;
     }
 
+    /// <summary>
+    /// The method to be run when an agent has been hit.
+    /// </summary>
+    /// <param name="agentInputHandler"></param>
+    /// <param name="position">The position when the agent was hit.</param>
+    /// <param name="normal">The normal of the hit position.</param>
+    /// <param name="value"></param>
     private void RunCommandOnAgentHasBeenHit(AgentInputHandler agentInputHandler, Vector3 position, Vector3 normal, float value)
     {
+        Debug.Log("AGENT HIT FEEDBACK");
+        
         GameObject agent = agentInputHandler.gameObject;
 
         if (agentInputHandler.agentHitSound != null)
@@ -29,9 +38,10 @@ public class AgentGivesHitFeedback : PassiveCommandObject
         {
             Debug.LogAssertion(agent.name + " doesn't have a hit feedback sound");
         }
-        
+
         if (agentInputHandler.agentHitParticles != null)
         {
+            Debug.Log("Instantiating hit effects!");
 			GameObject hitEffect = Instantiate(agentInputHandler.agentHitParticles, position, Quaternion.Euler(normal));
             Destroy(hitEffect, 5f);
         }
