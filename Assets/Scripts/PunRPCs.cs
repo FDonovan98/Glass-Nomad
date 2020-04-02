dfd0f3cccc486100074f8fde65f0e452e8497d07
HDonovan96 <harry.bd11@gmail.com>
2020-04-02 11:28:59 +0100

    Fixed shooting not happening over net, probably

---
+++ Assets/Scripts/PunRPCs.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+using Photon.Pun;
+
+public class PunRPCs : MonoBehaviourPunCallbacks
+{
+    [PunRPC]
+    public void Shoot(Vector3 cameraPos, Vector3 cameraForward, float weaponRange, int weaponDamage)
+    {     
+        Debug.Log("shoot");
+        RaycastHit hit;
+        if (Physics.Raycast(cameraPos, cameraForward, out hit, weaponRange))
+        {
+            Debug.Log(hit.transform.gameObject.name + " has been hit");
+            AgentInputHandler targetAgentInputHandler = hit.transform.gameObject.GetComponent<AgentInputHandler>();
+
+            Debug.Log(targetAgentInputHandler);
+            
+            if (targetAgentInputHandler.runCommandOnAgentHasBeenHit != null)
+            {
+                Debug.Log("hit feedback");
+                targetAgentInputHandler.runCommandOnAgentHasBeenHit(targetAgentInputHandler, hit.point, weaponDamage);
+            }
+        }
+    }
+}
