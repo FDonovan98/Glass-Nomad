c774bb357fd30f0e4bbd041a579161baceffdff8
Luke Day <ld227707@falmouth.ac.uk>
2020-04-03 16:17:21 +0100

    Actually added some functionality to the audio log trigger script.

--- Assets/Scripts/Miscellaneous/Trigger Scripts/AudioLogTriggerScript.cs
+++ Assets/Scripts/Miscellaneous/Trigger Scripts/AudioLogTriggerScript.cs
@@ -2,8 +2,11 @@
 
 public class AudioLogTriggerScript : TriggerInteractionScript
 {
+    [SerializeField] private AudioClip audioClip;
+
     protected override void InteractionComplete(GameObject player)
     {
+        Objectives.PlaySpeechAudio(audioClip);
         Debug.Log("Audio log picked up");
         Destroy(this.gameObject);
     }
