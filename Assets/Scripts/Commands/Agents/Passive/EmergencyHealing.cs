2fa08cffae5af1fe03ffa698e141658689b89580
HDonovan96 <harry.bd11@gmail.com>
2020-04-21 15:14:03 +0100

    Added blank EmergencyHealing command

---
+++ Assets/Scripts/Commands/Agents/Passive/EmergencyHealing.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DefaultEmergencyHealing", menuName = "Commands/Passive/Emergency Healing", order = 0)]
+public class EmergencyHealing : ScriptableObject
+{
+
+}
