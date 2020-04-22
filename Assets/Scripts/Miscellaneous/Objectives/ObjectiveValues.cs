b9fb9b67df6a6f2734d24f523308337a754a0188
Luke Day <ld227707@falmouth.ac.uk>
2020-04-22 16:22:30 +0100

    Changed the comments to tooltips so its easier to know what things do also changed the completed variable to hideininspector :)

--- Assets/Scripts/Miscellaneous/Objectives/ObjectiveValues.cs
+++ Assets/Scripts/Miscellaneous/Objectives/ObjectiveValues.cs
@@ -4,22 +4,22 @@ using System.Collections.Generic;
 [CreateAssetMenu(fileName = "DefaultObjectValues", menuName = "Commands/Objective Values")]
 public class ObjectiveValues : ScriptableObject
 {
-    // The main objective text to display
+    [Tooltip("The main objective text to display")]
     public string objectiveText = "INSERT OBJECTIVE TEXT HERE";
-    
-    // The shortened text to display permanently
+
+    [Tooltip("The shortened text to display permanently")]
     public string objectiveHint = "SMALLER TEXT AT THE TOP";
 
-    // The objective(s) that need to be completed before this one.
+    [Tooltip("The objective(s) that need to be completed before this one.")]
     public List<ObjectiveValues> requiredObjectives = new List<ObjectiveValues>();
 
-    // The sound/speech to play once the objective has been completed.
+    [Tooltip("The sound/speech to play once the objective has been completed.")]
     public AudioClip objectiveAudio = null;
 
-    // Has the objective been completed
-    public bool completed;
+    [Tooltip("Ths the objective been completed")]
+    [HideInInspector] public bool completed;
 
-    // The time, after the whole text is diplayed, for it to vanish.
+    [Tooltip("The time, after the whole text is diplayed, for it to vanish.")]
     public float timeToDisappear = 1f;
 
     public bool AllRequiredObjectivesCompleted()
