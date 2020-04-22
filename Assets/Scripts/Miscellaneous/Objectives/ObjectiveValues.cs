b2a6eb2321c6e0c90db2bcce78e0eda8aaf145b0
Luke Day <ld227707@falmouth.ac.uk>
2020-04-22 16:20:47 +0100

    Commented the variables in ObjectiveValues.

--- Assets/Scripts/Miscellaneous/Objectives/ObjectiveValues.cs
+++ Assets/Scripts/Miscellaneous/Objectives/ObjectiveValues.cs
@@ -4,11 +4,19 @@ using System.Collections.Generic;
 [CreateAssetMenu(fileName = "DefaultObjectValues", menuName = "Commands/Objective Values")]
 public class ObjectiveValues : ScriptableObject
 {
-    // The Objective's name will just be the name of the scriptable object.
+    // The main objective text to display
     public string objectiveText = "INSERT OBJECTIVE TEXT HERE";
+    
+    // The shortened text to display permanently
     public string objectiveHint = "SMALLER TEXT AT THE TOP";
+
+    // The objective(s) that need to be completed before this one.
     public List<ObjectiveValues> requiredObjectives = new List<ObjectiveValues>();
+
+    // The sound/speech to play once the objective has been completed.
     public AudioClip objectiveAudio = null;
+
+    // Has the objective been completed
     public bool completed;
 
     // The time, after the whole text is diplayed, for it to vanish.
