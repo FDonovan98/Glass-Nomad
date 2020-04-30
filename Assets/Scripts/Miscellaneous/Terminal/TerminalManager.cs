348958765eb0089d0d6249d751dfa2cc111c094f
Luke Day <ld227707@falmouth.ac.uk>
2020-05-01 00:20:34 +0100

    Added the loading dots animation and base scripting for the log selection in the terminal scene

--- Assets/Scripts/Miscellaneous/Terminal/TerminalManager.cs
+++ Assets/Scripts/Miscellaneous/Terminal/TerminalManager.cs
@@ -2,17 +2,21 @@
 using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TerminalManager : MonoBehaviour
 {
     [Header("Audio")]
     [SerializeField] private AudioSource audioSource = null;
+    [SerializeField] private AudioSource fanAudioSource = null;
+    [SerializeField] private AudioClip startUpSound = null;
     [SerializeField] private AudioClip fanWhirlSound = null;
     [SerializeField] private AudioClip typingSound = null;
-    [SerializeField] private AudioClip[] keyboardFoleySound;
+    [SerializeField] private AudioClip[] keyboardFoleySounds;
 
     [Header("UI Elements")]
     [SerializeField] private GameObject logInUI = null;
+    [SerializeField] private GameObject loadingUI = null;
     [SerializeField] private GameObject mainTerminalUI = null;
     
     [Header("Username")]
@@ -26,14 +30,24 @@ public class TerminalManager : MonoBehaviour
     private bool passwordCorrect = false;
 
     [Header("Logs")]
-    [SerializeField] private TerminalLog[] terminalLogs;
-    private TerminalLog currentLog = null;
+    [SerializeField] private GameObject[] terminalLogs;
+    [SerializeField] private Color selectedLogColor = Color.white;
+    private int currentLogIndex = 0;
+    
+    private bool menuControlsEnabled = false;
 
     // Private Variables
 
-    private void Start()
+    private async void Start()
     {
-        PlayAudioClip(fanWhirlSound);
+        PlayAudioClip(startUpSound);
+        await Task.Delay(TimeSpan.FromSeconds(3f));
+        fanAudioSource.clip = fanWhirlSound;
+        fanAudioSource.Play(); // ---> play constantly
+
+        // PLAY SCAN LINE ANIMATIONS
+
+        ToggleElement(logInUI);
         // fade in from black
         // fan audio whirl
         // enable password ui
@@ -55,8 +69,22 @@ public class TerminalManager : MonoBehaviour
         // load lobby
     }
 
+    private void PlayAudioClip(AudioClip audioClip)
+    {
+        if (audioClip == null) return;
+        audioSource.PlayOneShot(audioClip);
+    }
+
+    private void OnKeypressed()
+    {
+        if (keyboardFoleySounds.Length == 0) return;
+        AudioClip keyboardClip = keyboardFoleySounds[UnityEngine.Random.Range(0, keyboardFoleySounds.Length - 1)];
+        if (keyboardClip != null) PlayAudioClip(keyboardClip);
+    }
+
     public void CheckUsernameField(string usernameText)
     {
+        OnKeypressed();
         usernameCorrect = false;
         if (usernameText != correctUsername) return;
         Debug.Log("Username correct.");
@@ -70,6 +98,7 @@ public class TerminalManager : MonoBehaviour
 
     public void CheckPasswordField(string passwordText)
     {
+        OnKeypressed();
         passwordCorrect = false;
         if (passwordText != correctPassword) return;
         Debug.Log("Password correct.");
@@ -83,9 +112,89 @@ public class TerminalManager : MonoBehaviour
 
     private async void LogInSuccessful()
     {
+        // Disable Log In UI and enable the Loading UI
         await Task.Delay(TimeSpan.FromSeconds(1f));
         ToggleElement(logInUI);
+        ToggleElement(loadingUI);
+        CycleLoadingDots();
+        await Task.Delay(TimeSpan.FromSeconds(4f));
+        ToggleElement(loadingUI);
         ToggleElement(mainTerminalUI);
+        menuControlsEnabled = true;
+    }
+
+    private async void CycleLoadingDots()
+    {
+        TMP_Text loadingText = loadingUI.GetComponentInChildren<TMP_Text>();
+        for (int i = 0 ; i < 6; i++)
+        {
+            if (loadingText.text.Length > 10)
+            {
+                loadingText.text = "Loading.";
+                await Task.Delay(TimeSpan.FromSeconds(0.5f));
+                continue;
+            }
+            await Task.Delay(TimeSpan.FromSeconds(0.5f));
+            loadingText.text += ".";
+        }
+    }
+
+    private void Update()
+    {
+        if (!menuControlsEnabled) return;
+
+        MoveLogSelection(GetArrowInput());
+    }
+
+    private Vector3 GetArrowInput()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            return Vector2.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            return Vector2.down;
+        }
+        
+        return Vector2.zero;
+    }
+
+    private void MoveLogSelection(Vector2 arrowMovement)
+    {
+        if (arrowMovement == Vector2.zero) return;
+
+        // Deselect the current log
+        ToggleLogSelect(terminalLogs[currentLogIndex]);
+        
+        // Determine arrow movement
+        if (arrowMovement == Vector2.up)
+        {
+            ToggleLogSelect(GetNextLog(-1));
+        }
+        else if (arrowMovement == Vector2.down)
+        {
+            ToggleLogSelect(GetNextLog(1));
+        }
+    }
+
+    private GameObject GetNextLog(int dir)
+    {
+        if (dir != 1 || dir != -1) return null;
+        Debug.Log("Current Log Index: " + currentLogIndex);
+        currentLogIndex = (currentLogIndex + dir) % terminalLogs.Length - 1;
+        Debug.Log("New Log Index: " + currentLogIndex);
+        return terminalLogs[currentLogIndex];
+    }
+
+    private void ToggleLogSelect(GameObject log)
+    {
+        if (log.GetComponentInChildren<Image>().color == Color.clear)
+        {
+            log.GetComponentInChildren<Image>().color = selectedLogColor;
+            return;
+        }
+        log.GetComponentInChildren<Image>().color = Color.clear;
     }
 
     private async void TextScroll(string textToType, TMP_Text textElement, double timePerLetter = 0.1f, double timeToDisappear = 1f)
@@ -99,11 +208,6 @@ public class TerminalManager : MonoBehaviour
         }
         await Task.Delay(TimeSpan.FromSeconds(timeToDisappear));
         textElement.text = "";
-    }
-
-    private void PlayAudioClip(AudioClip audioClip)
-    {
-        audioSource.PlayOneShot(audioClip);
     }
 
     private void ToggleElement(GameObject elementToToggle)
